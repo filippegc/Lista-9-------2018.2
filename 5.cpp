cef07d2b052e9540eadd5b104457ae861a9c7536
filippegc <42630521+filippegc@users.noreply.github.com>
2018-09-05 21:16:21 -0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,66 +1,90 @@
-# include <iostream>
+	# include <iostream>
 
-using namespace std;
+	using namespace std;
 
-int main()
-{
-	setlocale(LC_ALL, "ptb");
-	int mes;
-	double mensalidade;
+	int main()
+	{
+		setlocale(LC_ALL, "ptb");
+		int mes;
+		double mensalidade;
 
-	cout << "Informe o mes em que a mensalidade est� sendo paga: ";
-	cin >> mes;
+		cout << "Informe o mes em que a mensalidade está sendo paga: ";
+		cin >> mes;
 
 
-	if (mes == 02 || mes == 2) {
-		mensalidade = 100 * 1.05 ;
-		cout << mensalidade << endl;
-	}
+		if (mes == 2) {
+			mensalidade = 100 * 1.05;
+			cout << mensalidade << endl;
+		}
 
-	else if (mes == 03 || mes == 3) {
-		mensalidade = 100 * 1.05 * 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 04 || mes == 4) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 05 || mes == 5) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 06 || mes == 6) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 07 || mes == 7) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 08 || mes == 8) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 09 ||  mes == 9) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 10 || mes == 10) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 11 || mes == 11) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else if (mes == 12 || mes == 12) {
-		mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
-		cout << mensalidade << endl;
-	}
-	else {
-		cout << "100" << endl;
-	}
+		else if (mes == 3) {
+			mensalidade = 100 * 1.05 * 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 4) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 5) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 6) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 7) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 8) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 9) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 10) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 11) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else if (mes == 12) {
+			mensalidade = 100 * 1.05 * 1.05* 1.05* 1.05* 1.05* 1.05* 1.05 * 1.05* 1.05* 1.05* 1.05;
+			cout << mensalidade << endl;
+		}
+		else {
+			cout << "100" << endl;
+		}
 
-	system("pause");
-	return 0;
-}
+
+
+
+		/* 
+		#include <iostream>
+		#include <cmath>
+		
+		using namespace std;
+		int main()
+		{
+			setlocale(LC_ALL,"ptb");
+			int mes;
+			float valor = 100;
+		
+		cout <<"Informe o mês do pagamento: ";
+		cin>>mes;
+		valor = valor *pow (1.05, mes-1);
+		cout<< "O valor é de " << valor <<endl;
+		system("pause");
+		return 0;
+		}
+		
+		*/
+
+		system("pause");
+		return 0;
+	}
